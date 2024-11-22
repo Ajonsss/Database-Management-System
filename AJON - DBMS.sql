55bea3475d5918306cd12212417246bbe2ef16e0
Andrae Ajon <21-06599@g.batstate-u.edu.ph>
2024-11-22 15:55:34 +0800

    Add files via upload

---
+++ AJON - DBMS.sql
@@ -0,0 +1,39 @@
+-- CREATE DATABASE DBMS;
+
+CREATE TABLE Friends (
+  namez VARCHAR(20) NOT NULL,
+  nickname VARCHAR(20) NOT NULL,
+  typa VARCHAR(20) NOT NULL,
+  solidrating INTEGER(2) NOT NULL
+);
+
+INSERT INTO Friends (namez, nickname, typa, solidrating) VALUES 
+('Angelo', 'Patal', 'Yapper', '10'),
+('David', 'Dabid', 'Lowkey', '10'),
+('Ronmel', 'Normel', 'Jokerist', '10'),
+('Angel Bert', 'Berto', 'Gamer', '10'),
+('Nino', 'Onin', 'Shorty', '10'),
+
+('Loyd', 'Loydi', 'Gamer', '10'),
+('Xander', 'Xander', 'Shorty', '10'),
+('Sherwin', 'Sherwin', 'Lowkey', '10'),
+('Rian', 'Rian', 'Racist', '10'),
+('Randhel', 'Delog', 'Yapper', '10'),
+
+('Coel', 'Wel', 'Yapper', '10'),
+('Adrian', 'Adrian', 'Gamer', '10'),
+('Ahmiel', 'Mutog', 'Yapper', '10'),
+('Lemuel', 'Lem', 'Loverboy', '10'),
+('Nheil', 'Nheil', 'Scholar', '10'),
+
+('Raven', 'Raven', 'Loverboy', '10'),
+('Marjorie', 'Majo', 'Yapper', '10'),
+('Isabela', 'Isa', 'Yapper', '10'),
+('Luraine', 'Ure', 'Yapper', '10'),
+('Yhuan', 'Dominic', 'Tiktoker', '10'),
+
+('Reden', 'Madam', 'Girl Boss', '10'),
+('Lovely', 'Mam Love', 'Academic', '10'),
+('Lyca', 'Mata', 'YLoverboi', '10'),
+('Ashly', 'Sabel', 'Yapper', '10'),
+('Janine', 'Anin', 'Lowkey', '10');
